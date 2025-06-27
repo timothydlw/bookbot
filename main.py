934b18642d788725f164928615bb3bcb9bf1a1d3
timothydlw <watsonltim@gmail.com>
2025-06-27 10:43:49 -0500

    Add Character Counter

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from stats import num_of_words_in_book
 
+from stats import num_of_times_char_appear_in_book
+
 def get_book_text(input):
     with open(input) as f:
     # do something with f (the file) here
@@ -10,7 +12,9 @@ def get_book_text(input):
 def main():
     file_contents = get_book_text("books/frankenstein.txt")
     total_words = num_of_words_in_book("books/frankenstein.txt")
+    character_totals = num_of_times_char_appear_in_book("books/frankenstein.txt")
     print (file_contents)
     print(f"{total_words} words found in the document")
+    print(character_totals)
 
 main()
--- stats.py
+++ stats.py
@@ -4,3 +4,15 @@ def num_of_words_in_book(input):
         words = file_contents.split()
         total_words = len(words)
     return total_words
+
+def num_of_times_char_appear_in_book(input):
+    character_totals = {}
+    with open(input) as f:
+        file_contents = f.read()
+        lower_case_string = file_contents.lower()
+        for character in lower_case_string:
+            if character in character_totals:
+                character_totals[character] += 1
+            else:
+                character_totals[character] = 1
+        return character_totals
